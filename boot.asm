a66a3deedf5bba7916055acc99ff1f48209d9d39
hrchlhck <vpemfh7@gmail.com>
2021-04-09 01:43:17 -0300

    Added comments

--- boot.asm
+++ boot.asm
@@ -1,29 +1,30 @@
-[BITS 16]
-[ORG 0x7c00]
+[BITS 16]               ; Operating with 16 bit values? 
+[ORG 0x7c00]            ; Memory address where the code will be started
 
 start:
-    mov si, message
-    call print
-    jmp $
+    mov si, message     ; Moves the address of 'message' into si
+    call print          ; Call print
+    jmp $               ; Eternally repeat start
 
 print:
-    mov bx, 0
+    mov bx, 0 
+
 .loop:
-    lodsb 
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
+    lodsb               ; Load a char into AL
+    cmp al, 0           ; Compare if it's empty
+    je .done            ; if empty, jump to done
+    call print_char     ; else, call print_char
+    jmp .loop           ; Repeat until reach null terminator of 'message'
 
 .done:
-    ret
+    ret                 ; Pop address from stack
 
 print_char:
-    mov ah, 0eh
-    int 0x10
-    ret
+    mov ah, 0eh         ; Print function code
+    int 0x10            ; Call BIOS
+    ret                 ; Pop address from stack
 
-message: db 'Hello, world!', 0
+message: db 'Hello, world!', 0 ; Creates a label to 'Hello, world' bytes and ends with a null terminator (0)
 
-times 510-($ - $$) db 0
-dw 0xaa55
+times 510-($ - $$) db 0    ; Fills the rest of the sector with 0
+dw 0xaa55                  ; Means that the code is bootable
